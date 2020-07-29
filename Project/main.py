8b3a470d8d4c73081cf557bedc7b1543e2cd6dff
User <you@example.com>
2020-07-29 14:24:44 +0500

    update

---
+++ Project/main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+import flask
+import os
+import random
+
+app = flask.Flask(
+	__name__,
+	static_folder='static/',
+	template_folder='templates/'
+)
+
+@app.route("/")
+def index():
+	return flask.render_template('index.html')
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=4567, debug=True)
---
+++ README.md
@@ -0,0 +1,3 @@
+Приложение - таймер
+---
+[![BCH compliance](https://bettercodehub.com/edge/badge/peshcom/cicd-timer?branch=master)](https://bettercodehub.com/)
