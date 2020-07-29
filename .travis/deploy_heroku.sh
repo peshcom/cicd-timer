8ee042ca54939951268c7c104dfe3715f482d920
User <you@example.com>
2020-07-29 19:57:32 +0500

    update

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -5,3 +5,4 @@ heroku container:login
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
+heroku run rails console 
