f6770366e48a42186b2baf91e06005bd32932ce8
User <you@example.com>
2020-08-08 02:38:17 +0500

    update

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t gagarin99/cicd-timer:$TAG .
-docker push gagarin99/cicd-timer
+TAG="latest"
+OUTPUT_Docker=$DOCKER_USER"/cicd-timer"
+
+docker login --username $DOCKER_USER --password $DOCKER_PASS
+docker build -f Dockerfile -t $OUTPUT_Docker:$TAG .
+docker push $OUTPUT_Docker
